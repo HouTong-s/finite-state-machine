1108313c2c224740c87820fe3ff5c3f235813ef3
侯彤 <49677236+HouTong-s@users.noreply.github.com>
2023-02-08 12:50:50 +0800

    Create 有限状态机.cpp

--- 有限状态机.cpp
+++ 有限状态机.cpp
@@ -1,4 +1,4 @@
-﻿// 有限状态机.cpp 
+// 有限状态机.cpp 
 //
 /*
 消除多余空格
@@ -16,6 +16,7 @@
 #include<string>
 #include<map>
 #include<vector>
+
 using namespace std;
 
 /*
@@ -77,62 +78,66 @@ bool transfer_state(int & n,char event,string & result)
     return true;
 }
 
+//状态枚举类
+enum class Status
+{
+    //三个状态，用A、B、C来指代0、1、2
+    A = 0,B,C
+};
 //下面用一个结构体来表示状态转移
 struct fsm_item
 {
     //status为当前状态码
-    int status;
-    //map的键表示event，值表示该event对应的后一个状态
-    //键为'#'时，表示默认事件(default event)的下一个状态；也可无该键，这取决于具体需求
-    map<char,int> transfers;
-    fsm_item(int s,const map<char, int>& trfs)
+    Status status;
+    /*
+    map的键表示event，值表示该event对应的后一个状态
+    键为'#'时，表示默认事件(default event)的下一个状态；也可无该键，这取决于具体需求
+    */
+    map<char,Status> transfers;
+    fsm_item(Status s,const map<char, Status>& trfs)
     {
         status = s;
         transfers = trfs;
     }
 };
 //状态转移表
-const vector<fsm_item*> items = { new fsm_item(0,map<char,int>{ {' ',1},{'\"',2},{'#',0} }),
-                                  new fsm_item(1,map<char,int>{ {' ',1},{'\"',2},{'#',0}}),
-                                  new fsm_item(2,map<char,int>{ {'\"',0},{'#',2}}),};
+const vector<fsm_item*> items = { new fsm_item(Status::A,map<char,Status>{ {' ',Status::B},{'\"',Status::C},{'#',Status::A} }),
+                                  new fsm_item(Status::B,map<char,Status>{ {' ',Status::B},{'\"',Status::C},{'#',Status::A}}),
+                                  new fsm_item(Status::C,map<char,Status>{ {'\"',Status::A},{'#',Status::C}}),};
 
 /*
 * 利用结构体的状态转移函数，便于扩展，但原理是按照上一个函数来的，不过看起来更工整3
 * n为当前状态码，event为当前的动作(或者说是事件)
 * 如果有错误的event，则返回false；否则返回true
 */
-bool struct_transfer_state(int& n, char event, string& result)
+bool struct_transfer_state(Status& current_status, char event, string& result)
 {
     //是否是错误的状态转移，这里不存在错误的状态转移
     bool isFalseTransfer = true;
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i]->status == n)
+        if (items[i]->status == current_status)
         {
             //属于状态转移表中的状态，设为false
             isFalseTransfer = false;
-            //是否是默认的event
-            bool isDefaultEvent = true;
+
             for (auto transfer : items[i]->transfers)
             {
                 if (event == transfer.first)
-                {
-                    isDefaultEvent = false;                 
+                {              
                     //特异性需求，这里在如下情况下result后面要添加字符
-                    if (n != 1 || event != ' ')
+                    if (current_status != Status::B || event != ' ')
                     {
                         result.push_back(event);
                     }
-                    n = transfer.second;
-                    break;
+                    current_status = transfer.second;
+                    return true;
                 }              
             }
-            if (isDefaultEvent)
-            {
-                //也是特异性需求
-                result.push_back(event);
-                n = items[i]->transfers['#'];
-            }     
+            //默认情况的状态转移         
+            current_status = items[i]->transfers['#'];   
+            //这里也是特异性需求
+            result.push_back(event);
             return true;
         }
     }
@@ -145,30 +150,27 @@ bool struct_transfer_state(int& n, char event, string& result)
 }
 
 //纯状态转移，只考虑状态的迁移，不涉及result的改变(可作为其他自动机的雏形)
-bool pure_struct_transfer_state(int& n, char event)
+bool pure_struct_transfer_state(Status& current_status, char event)
 {
     //是否是错误的状态转移，这里不存在错误的状态转移
     bool isFalseTransfer = true;
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i]->status == n)
+        if (items[i]->status == current_status)
         {
             //属于状态转移表中的状态，设为false
             isFalseTransfer = false;
-            //是否是默认的event
-            bool isDefaultEvent = true;
 
             for (auto transfer : items[i]->transfers)
             {
                 if (event == transfer.first)
                 {
-                    isDefaultEvent = false;
-                    n = transfer.second;
+                    current_status = transfer.second;
+                    return true;
                 }
             }
-            if (isDefaultEvent)
-                n = items[i]->transfers['#'];
-            //这里有默认event转移，所有可以直接返回
+            //这里有默认event转移，所有可以返回true,否则这里返回false
+            current_status = items[i]->transfers['#'];      
             return true;
         }
     }
@@ -183,11 +185,13 @@ int main()
 {
     string s = "abc   \"cde  eff\"    asd";
     //getline(cin, s);
-    int n = 0;
+    
+    //初始状态为A
+    Status status = Status::A;
     string result = "";
     for (int i = 0; i < s.size(); i++)
     {
-        if (!struct_transfer_state(n, s[i],result))
+        if (!struct_transfer_state(status, s[i],result))
         {
             cout << "error" << endl;
             exit(1);
@@ -195,7 +199,7 @@ int main()
         else
         //打印状态转移的序列
         {
-            cout << n << " ";
+            cout << (int)status << " ";
         }
     }
     cout << endl << result << endl;
